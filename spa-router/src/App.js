9ae1408648e9e1faf462326d7b201b780d1f46e9
JongHyeon <sjh2273@naver.com>
2021-10-27 23:55:42 +0900

    spa updated

---
+++ spa-router/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Rooms from './Rooms';
+
+const Home = () => {
+    return <h2>이 곳은 홈페이지입니다.</h2>
+}
+const Photo = () => {
+    return <h2>여기서 사진을 감상하세요.</h2>
+}
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div style={{ padding: 20, border: '5px solid gray' }}>
+            <Link to='/'>홈</Link>
+            <br/>
+            <Link to='/photo'>사진</Link>
+            <br/>
+            <Link to='/rooms'>방 소개</Link>
+            <br/>
+            <Route exact="exact" path='/' component={Home}/>
+            <Route path='/photo' component={Photo}/>
+            <Route path='/rooms' component={Rooms}/>
+            </div>
+        </BrowserRouter>
+    );
+}
+export default App;
---
+++ spa-router/src/Rooms.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route,Link } from 'react-router-dom';
+
+const Rooms = ({ match }) => {
+    return (
+        <div>
+            <h2>여기는 방을 소개하는 페이지입니다.</h2>
+            <Link to={`${match.url}/blueRoom`}>파란 방입니다.</Link>
+            <br />
+            <Link to={`${match.url}/greenRoom`}>초록 방입니다.</Link>
+            <br />
+            <Route exact path={match.url} render={() => <h3>방을 선택해 주세요.</h3>} />
+            <Route path={`${match.url}/:roomId`} component={Room} />
+        </div>
+    )
+}
+
+const Room = ({ match }) => {
+    return <h2>{`${match.params.roomId} 방을 선택하셨습니다.`}</h2>
+}
+export default Rooms
